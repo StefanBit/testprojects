996ada7dddd24baf4833b13ae56fa7cd96cb096c
sbili <sbili@DESKTOP-JESKJ72>
2017-09-02 16:57:31 +0200

    new Stage for Chart and some db

---
+++ TradeFX/src/view/ChartStage.java
@@ -0,0 +1,21 @@
+package view;
+
+import java.util.ArrayList;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import model.HistData;
+
+public class ChartStage extends Stage{
+public ChartStage(ArrayList<HistData> data) {
+	HBox hBox = new HBox();
+	ChartView chartview = new ChartView();
+	chartview.setData(data);
+	Scene SymbolScene = new Scene(hBox, 800, 400);
+	setScene(SymbolScene);
+	setTitle("StockStage");
+	hBox.getChildren().add(chartview.lineChart);
+	show();
+}
+}
